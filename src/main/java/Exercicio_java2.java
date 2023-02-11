1eab0d85b01521105c9979300b1ff1789c5f85ad
luizfelipegtr <Luizfelipegtr@brq.com>
2023-02-10 22:30:30 -0300

    commit 3

--- src/main/java/Exercicio_java2.java
+++ src/main/java/Exercicio_java2.java
@@ -1,9 +1,28 @@
+import javax.swing.*;
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Exercicio_java2 {
 
     public static void main(String[] args) {
+        System.out.println("insira seu login");
+        Scanner input1 = new Scanner(System.in);
+        String nome = input1.nextLine();
+        if (nome.equalsIgnoreCase("andrei")){
 
-        System.out.println();
+            System.out.println("insira sua senha");
+            Scanner input2 = new Scanner(System.in);
+            String senha = input2.nextLine();
+            if (senha.equalsIgnoreCase("dev_mais_fraco_do_mundo"));
+            System.out.println("cadê a bunfunfa?");
+            Scanner input3 = new Scanner(System.in);
+            String resposta = input3.nextLine();
+            if (resposta.equalsIgnoreCase("não sei");
+            System.out.println();
 
+
+        }
 
     }
 

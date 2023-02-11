467942e18cbdb7d19b299e60295d0d81ef786883
luizfelipegtr <Luizfelipegtr@brq.com>
2023-02-11 11:16:31 -0300

    commit if else 3

--- src/main/java/Exercicio_java2.java
+++ src/main/java/Exercicio_java2.java
@@ -9,24 +9,20 @@ public class Exercicio_java2 {
         System.out.println("insira seu login");
         Scanner input1 = new Scanner(System.in);
         String nome = input1.nextLine();
-        if (nome.equalsIgnoreCase("andrei")){
+        if (nome.equalsIgnoreCase("andrei")) {
 
             System.out.println("insira sua senha");
             Scanner input2 = new Scanner(System.in);
             String senha = input2.nextLine();
-            if (senha.equalsIgnoreCase("12032006"));
+            if (senha.equalsIgnoreCase("7/12/2006"));
             System.out.println("cadê a bunfunfa?");
-            Scanner input3 = new Scanner(System.in);
-            String resposta = input3.nextLine();
-            if (resposta.equalsIgnoreCase("não sei");{
-                System.out.println("será");
             }
+        Scanner input3 = new Scanner(System.in);
+        String A1 = input3.nextLine();
+        System.out.println("top da balada");
 
 
         }
 
     }
 
-
-
-}
